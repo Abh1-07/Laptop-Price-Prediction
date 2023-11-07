6025bb2141c27c0fd3d6a7f052cd0c4d4aaf9a83
Abhishek Tiwari <vedanshtiwari.07@gmail.com>
2023-11-07 13:18:43 +0530

    Model Training

---
+++ src/components/model_trainer.py
@@ -0,0 +1,63 @@
+import os
+import sys
+from src.logger import logging
+from src.exception import CustomException
+from src.utils import evaluate_model
+from src.utils import save_object
+from dataclasses import dataclass
+#models
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import AdaBoostRegressor, GradientBoostingRegressor, RandomForestRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from catboost import CatBoostRegressor
+from xgboost import XGBRegressor
+from sklearn.metrics import r2_score
+
+
+@dataclass
+class Model_trainer_config:
+    model_trainer_obj_path = os.path.join('Artifacts','model.pkl')
+
+    
+class Model_trainer:
+    
+    def __init__(self):
+        self.model_trainer_config = Model_trainer_config()
+
+    def initiate_model_trainer(self, train_array, test_array):
+        try:
+            x_train,x_test,y_train,y_test = (
+                train_array[:,:-1],
+                train_array[:,-1],
+                test_array[:,:-1],
+                test_array[:,-1]
+            )
+            models = {
+                'Linear Regression': LinearRegression(),
+                'K-Neighbors Regressor': KNeighborsRegressor(),
+                'Decision Tree': DecisionTreeRegressor(),
+                'Random Forest Regressor': RandomForestRegressor(),
+                'XGBRegressor': XGBRegressor(),
+                "CatBoosting": CatBoostRegressor(verbose = False),
+                'Ada Boosting': AdaBoostRegressor()
+            }
+
+            models_report: dict = evaluate_model(x_train, y_train, x_test, y_test, models)
+
+            best_model_score = max(sorted(list(models_report.values())))
+            best_model_name = list(models_report.keys())[list(models_report.values()).index(best_model_score)]
+            best_model = models[best_model_name]
+
+            if best_model_score < 0.6:
+                raise CustomException("No Best Model Found!")
+            save_object(
+                file_path=self.model_trainer_config.model_trainer_obj_path,
+                obj = best_model
+            )
+            predicted_best_model = best_model.predict(x_test)
+            r2_square = r2_score(y_test, predicted_best_model)
+            return r2_square
+        except Exception as e:
+            raise CustomException(e, sys)
+        
---
+++ src/utils.py
@@ -0,0 +1,35 @@
+import os 
+import sys
+import dill
+from src.logger import logging
+from src.exception import CustomException
+from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
+import numpy as np
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        # saving the pickle model to desired place
+        os.makedirs(dir_path,exist_ok=True)
+        with open(file_path,'wb') as file_obj:
+            dill.dump(obj,file_obj)
+        logging.info('Pickle file made and dumped with data')
+    except Exception as e:
+        raise CustomException(e,sys)    
+
+def evalute_model(x_train,y_train,x_test,y_test,models):
+    try:
+        report = {}
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            model.fit(x_train,y_train)
+
+            y_predict_train = model.predict(x_train)
+            y_predict_test = model.predict(y_train)
+
+            model_train_r2sq = r2_score(y_train, y_predict_train)
+            model_test_r2sq = r2_score(y_test, y_predict_test)
+            report[list(models.values())[i]] = model_test_r2sq
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
